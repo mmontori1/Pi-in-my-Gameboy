ed1810c8c5d10037116b127e833787adb7a0f500
Mariano <mmontori@umich.edu>
2017-02-25 12:57:02 -0600

    Added functionality of buttons
    —— App ——
    -now has two button, emulate and exit
    -emulate button runs a line in bash
    —— GUI Interface ——
    -app size now is same as raspberry pi screen
    -improved background with consistent coloring

--- gui.py
+++ gui.py
@@ -1,16 +1,24 @@
 from Tkinter import *
 import Tkinter
+import subprocess
+
+bgcolor = "#2143a2"
 
 # to do: have a shell script to run which this python function will call and run on terminal
 def emulationScript():
 	print("huh this ain't an emulator")
+	subprocess.Popen(['ls', '-l'])
 
 def main():
 	root = Tk()
-	frame = Frame(root, width = 400, height = 400, background = "#2143a2")
-	retro = Button(frame, text="Run RetroPie?", bg = "#2143a2", command = emulationScript)
-	retro.pack(padx = 100, pady = 100)
+	root.geometry("268x201")
+	root.configure(background = bgcolor)
+	frame = Frame(root, background = bgcolor)
 	frame.pack()
+	retro = Button(frame, text="Run RetroPie?", highlightbackground = bgcolor, command = emulationScript)
+	retro.pack(padx = 67, pady = 50)
+	exit = Button(frame, text="exit", highlightbackground = bgcolor)
+	exit.pack()
 	root.mainloop()
 
 if __name__ == "__main__":
