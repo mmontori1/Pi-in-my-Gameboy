75400a3ae30336a6f4ac0a514c5109d8f815a40f
Mariano <mmontori@umich.edu>
2017-02-25 05:02:42 -0600

    started work on window frame
    ——  Frame ——
    -added background
    -button padding
    —— Other ——
    -restructured code
    -added logo (to add later)

--- gui.py
+++ gui.py
@@ -1,13 +1,17 @@
 from Tkinter import *
-import tkMessageBox
 import Tkinter
 
-root = Tkinter.Tk()
 # to do: have a shell script to run which this python function will call and run on terminal
 def emulationScript():
 	print("huh this ain't an emulator")
 
-retro = Tkinter.Button(root, text="Run RetroPie?", command = emulationScript)
-retro.pack()
+def main():
+	root = Tk()
+	frame = Frame(root, width = 400, height = 400, background = "#2143a2")
+	retro = Button(frame, text="Run RetroPie?", bg = "#2143a2", command = emulationScript)
+	retro.pack(padx = 100, pady = 100)
+	frame.pack()
+	root.mainloop()
 
-root.mainloop()
+if __name__ == "__main__":
+	main()
