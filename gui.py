2ecfae5779ce8ad31ad5d4d06a14bff20b332617
Mariano <mmontori@umich.edu>
2017-02-25 19:49:39 -0600

    Added extra error checking
    -now goes through and checks if file exists as well
    -more descriptive erros
    -code block comments

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 import Tkinter
+import os
 import subprocess
 import tkMessageBox
 from PIL import Image, ImageTk
@@ -7,6 +8,7 @@ from PIL import Image, ImageTk
 bgcolor = "#2143a2"
 root = Tk()
 
+# animated gif logo 
 class logo(Label):
 	def __init__(self, master, filename, background):
 		anim = Image.open(filename)
@@ -15,6 +17,7 @@ class logo(Label):
 		self.index = 0
 		self.delay = anim.info['duration']
 
+		#append until at end
 		try:
 			while True:
 				gif.append(anim.copy())
@@ -22,6 +25,7 @@ class logo(Label):
 		except EOFError:
 			pass
 
+		#convert and adds all frames
 		self.first = gif[0].convert('RGBA')
 		self.frames.append(ImageTk.PhotoImage(self.first))
 
@@ -32,6 +36,7 @@ class logo(Label):
 			self.frame = next.convert('RGBA')
 			self.frames.append(ImageTk.PhotoImage(self.frame))
 
+		#loops through and reconfigures GUI with a new gif frame
 		loop = self.after(self.delay, self.play)
 
 	def play(self):
@@ -44,10 +49,16 @@ class logo(Label):
 # tries to run emulate.sh, else prints out error message
 def emulationScript():
 	try:
-		subprocess.call(['sh', 'emulate.sh'])
-		root.destroy()
+		# run if the file exists
+		if(os.path.exists('emulate.sh')):
+			subprocess.call(['sh', 'emulate.sh'])
+			root.destroy()
+		# error show if not found or in directory
+		else:
+			tkMessageBox.showinfo("huh this ain't an emulator", "Error: Shell Script not found in directory!")
+	#error with the subprocess call
 	except:
-		tkMessageBox.showinfo("huh this ain't an emulator", "Error: Shell Script ran into an error and couldn't run")
+		tkMessageBox.showinfo("huh this ain't an emulator", "Error: subprocess call unrecognized")
 
 # exits the app to Raspbian
 def exitApp():
@@ -55,21 +66,26 @@ def exitApp():
 
 # runs app
 def main():
+	#size, color, title
 	root.geometry("268x201")
 	root.configure(background = bgcolor)
 	root.wm_title("Pi in my Gameboy")
 	
+	#logo animation
 	anim = logo(root, "Rasp_turn_around.gif", background = bgcolor)
 	anim.pack()
 
+	#window frame to add buttons
 	frame = Frame(root, background = bgcolor)
 	frame.pack()
 
+	#buttons
 	retro = Button(frame, text="Run RetroPie", highlightbackground = bgcolor, command = emulationScript)
 	retro.pack()
 	exit = Button(frame, text="Exit to Raspbian", highlightbackground = bgcolor, command = exitApp)
 	exit.pack()
 
+	#GUI loop
 	root.mainloop()
 
 if __name__ == "__main__":
